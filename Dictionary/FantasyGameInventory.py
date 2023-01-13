d7c382caf5a55d223f613b7515fbfa6a5820d256
spellsharp <svcodin22@gmail.com>
2023-01-13 11:02:34 +0530

    inventory done

--- Dictionary/FantasyGameInventory.py
+++ Dictionary/FantasyGameInventory.py
@@ -1,4 +1,5 @@
-stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+import copy
+# stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 
 
 def displayInventory(inventory):
@@ -11,6 +12,32 @@ def displayInventory(inventory):
     print()
     print("Total number of items: %d" % (count))
 
-displayInventory(stuff)
+# displayInventory(stuff)
 
-    
+#########################################################################
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+def addToInventory(inventory, addedItems):
+    lootList = [*set(addedItems)] #remove duplicates
+    newinv = copy.deepcopy(inventory)
+    for item, number in inventory.items():
+        for loot in lootList:       
+            #finding number of existing elements
+            lootCount = addedItems.count(loot)    
+                 
+            if loot == item:
+                #update existing items numbers
+                newinv[item] += lootCount
+
+            if loot not in inventory:
+                newinv.setdefault(loot,str(lootCount)) #add the item if not in dict
+            lootCount = 1
+
+    inventory = copy.copy(newinv)
+    return inventory
+
+x = addToInventory(inv,dragonLoot) 
+
+displayInventory(x)
