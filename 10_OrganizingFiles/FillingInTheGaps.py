d7b5714f52a26347a6209f43dce72a707bccaa39
spellsharp <svcodin22@gmail.com>
2023-01-18 01:29:08 +0530

    README updated

--- 10_OrganizingFiles/FillingInTheGaps.py
+++ 10_OrganizingFiles/FillingInTheGaps.py
@@ -8,11 +8,13 @@ pattern = re.compile(r'^(.*?)(\d+)(.*?)$')
 
 file_num = []
 
+
 for folderName, subFolders, filenames in os.walk(user_folder):
     for filename in filenames:
         sample = pattern.search(filename)
         num = int(sample.group(2))
         file_num.append(num)
+        another_num.append(num)
 
 file_num.sort()
 print(file_num)
@@ -26,16 +28,19 @@ for i in range(1,length):
 
 print(file_num)
 
+
 for folderName, subFolders, filenames in os.walk(user_folder):
     for filename in filenames:
+
         sample = pattern.search(filename)
         before = sample.group(1)
         num = sample.group(2)
         after = sample.group(3)
         current_path = user_folder + '/' + filename
-        print(file_num[i],end=',')
+
         new_path = user_folder + '/' + before + str(file_num[i]) + after
         # shutil.move(current_path, new_path)
+        
         print("originally: " + current_path)
         print("renamed to: " + new_path)
         print()
