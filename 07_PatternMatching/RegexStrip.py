39e401a48fa8cfefa3c91facc16989308af4de98
spellsharp <svcodin22@gmail.com>
2023-01-17 12:02:37 +0530

    Finished regexStrip atlastgit add .

--- 07_PatternMatching/RegexStrip.py
+++ 07_PatternMatching/RegexStrip.py
@@ -1,15 +1,20 @@
 #need to finish
 import re
-x = '        hello world         '
 
-for i in range(len(x)):
-    if x[i]==' ':
-        continue
-    elif x[i] != ' ':
-        lindex = i
-        break
-for i in range(len(x)-lindex-1):
-    if x[i+lindex] == ' ':
-        continue
+def strip(user_input, c=""):
+    if c == "":
+        pattern = re.compile(r'^\s+|\s+$') #we check input endswith or/and starts with space or tab etc.
+        new_strip = pattern.sub("",user_input)
+    else:
+        no_space = strip(user_input, "") #we need to first remove any spaces before removing the character.
+        pattern = re.compile(rf'^{c}+|{c}+$')#we check input endswith or/and starts with given string.
+        new_strip = pattern.sub("", no_space)
     
+    return new_strip
+print()
+string = input("input string to be stripped: ")
+strip_char = input("character do you want to strip: ")
+
+print(strip(string,strip_char))
+
 
