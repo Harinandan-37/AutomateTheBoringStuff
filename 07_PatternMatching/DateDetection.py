9a2b0e6fc4b4448aa18e06af0aa02047037362ee
spellsharp <svcodin22@gmail.com>
2023-01-30 23:27:25 +0530

    Ch7 Date Detection corrected

--- 07_PatternMatching/DateDetection.py
+++ 07_PatternMatching/DateDetection.py
@@ -1,11 +1,38 @@
 import re
 try:
     date = input("Enter a date: ")
-    pattern = re.compile(r'^(3[01]|[1-2][0-9]|0[1-9])\/(1[0-2]|0[1-9])\/(0{3}[1-9]|0{2}[1-9]{2}|[0-1]{2}[1-9]{2}|2[0-9]{3})$')
-    0
-    sample = pattern.findall(date)
-    for i in range(len(sample[0])):
-        print(sample[0][i], end = ' ')
+    pattern = re.compile(r'^(3[01]|[1-2][0-9]|0[1-9])/(1[0-2]|0[1-9])/(0{3}[1-9]|0{2}[1-9]{2}|[0-1]{2}[1-9]{2}|2[0-9]{3})$')
+
+    sample = pattern.search(date)
+
+    day = int(sample.group(1))
+    month = int(sample.group(2))
+    year = int(sample.group(3))
+    
     print()
-except:
+
+    if month == 2:
+        if year%4==0 or year%400==0:
+            if day>29:
+                print("Invalid <dd> for February ")
+        else:
+            if day > 28:
+                print("Invalid <dd> for February")
+            
+
+    if day > 31:
+        print("Invalid <dd> ")
+
+    if month == 2 or month == 4 or month == 6 or month == 9 or month == 11:
+        if day > 30:
+            print("Invalid <dd> for given month.")
+
+    print()
+
+    for i in range(len(sample[0])):
+        print(sample[0][i], end = '')
+        
+    print()
+
+except AttributeError:
     print("Enter an appropriate date in format <dd/mm/yyyy>")
