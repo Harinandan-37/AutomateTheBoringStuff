3694aa9282086974185bda5df9b984894616f37a
spellsharp <svcodin22@gmail.com>
2023-02-03 09:59:23 +0530

    Updated README for Ch14 and Ch18

--- 18_SendingGmailAndTextMessages/randomChore.py
+++ 18_SendingGmailAndTextMessages/randomChore.py
@@ -1,7 +1,11 @@
 import ezgmail
 import random
 
+
 emailDict = {'sharan' : 'sharanv021104@gmail.com', 'svcodin' : 'svcodin22@gmail.com', 'shrisharanyan': 'shrisharanyan.clg@gmail.com'}
+# I CAN ALSO READ A FILE CONTAINING THE NAMES AND EMAILS.
+# I WOULD USE THE .readlines() TO GET A LIST OF ALL THE LINES.
+# I WOULD THEN DIFFERENTIATE THE NAME AND EMAIL BY REGEX. I COULD DEFINE A REGULAR EXPRESSION THAT A GENERAL EMAIL WOULD HAVE.
 
 nameList = ['sharan','svcodin','shrisharanyan']
 
